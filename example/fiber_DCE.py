c6899c69cf1f99e13d7371d324fe12bc3f95710d
mfrodrigo <rodrigomf43@gmail.com>
2021-06-13 13:40:41 -0300

    udpdate example

---
+++ example/fiber_DCE.py
@@ -0,0 +1,101 @@
+# test Channel
+
+# INPUT
+#
+# u0 - starting field amplitude (vector)
+# dt - time step
+# dz - propagation stepsize
+# nz - number of steps to take, ie, ztotal = dz*nz
+# alpha - power loss coefficient, ie, P=P0*exp(-alpha*z)
+# betap - dispersion polynomial coefs, [beta_0 ... beta_m]
+# gamma - nonlinearity coefficient
+# maxiter - max number of iterations (default = 4)
+# tol - convergence tolerance (default = 1e-5)
+#
+# OUTPUT
+#
+# u1 - field at the output
+
+# Libraries
+
+import math
+import numpy as np
+import pandas as pd
+from scipy import signal
+from Channel.channel import Channel
+from pulse.half_power import return_half_power
+import matplotlib.pyplot as plt
+
+# dt
+T = 1000  # (ps) deve ser pelo 4x FWHM
+num_samplesperbit = 2 ** 11  # should be 2^n
+dt = T / num_samplesperbit  # sampling time(ps) # time step (ps)
+t = (np.array(range(1, num_samplesperbit + 1)) - (num_samplesperbit + 1) / 2) * dt
+
+# Pulse
+FWHM = 100
+t0 = 0
+C = 0
+m = 1
+P0 = 1
+u0 = np.zeros(shape=(len(t), 1), dtype=complex)
+u0[:, 0] = math.sqrt(P0) * 2 ** (-((1 + 1j * C) / 2) * (2 * (t - t0) / FWHM) ** (2 * m))
+dz = 0.5  # distance stepsize (km)
+
+# Fiber 1
+beta2 = -43  # beta2 (ps^2/km)
+betap = np.transpose(np.array([0, 0, beta2]).reshape(1, 3))  # dispersion polynomial
+gamma = 0.01
+alpha = 0.05
+
+# DCE Fiber
+
+
+nz_step = range(40, 401, 40)
+list_output_1 = []
+list_delta_1 = []
+for nz in nz_step:
+    # output
+    u1 = Channel.ssprop(u0, dt, dz, nz, alpha, betap, gamma)
+
+    print('###########################################################')
+    list_values = return_half_power(t, u1)
+    print(nz * dz, list_values)
+    list_output_1.append(list_values[3])
+    list_delta_1.append(list_values[2])
+    print('###########################################################')
+    fig = plt.figure()
+    plt.plot(t, abs(u0[:, 0]) ** 2, label='Input')
+    plt.plot(t, abs(u1[:, 0]) ** 2, label='Output')
+    plt.title('Gaussian Pulse ')
+    plt.xlabel(r'$(t-\beta_1z)/T_0$')
+    plt.ylabel('|u1(z,t)|^2/P_0')
+    plt.legend()
+    plt.grid(True)
+    fig.savefig('Plot canal: ' + str(nz * dz) + 'Km, alpha = ' + str(alpha) + '_beta_2_'
+                + str(beta2) + '_gamma_' + str(gamma) + '.png', dpi=fig.dpi)
+
+fig, ax1 = plt.subplots()
+color = 'red'
+ax1.set_xlabel('distance (km)')
+ax1.set_ylabel('Output Power', color=color)
+ax1.plot(np.array(nz_step) * dz, np.array(list_output_1) / P0 * 100, color=color)
+ax1.tick_params(axis='y', labelcolor=color)
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+color = 'blue'
+ax2.set_ylabel('Delta', color=color)  # we already handled the x-label with ax1
+ax2.plot(np.array(nz_step) * dz, list_delta_1, color=color)
+ax2.tick_params(axis='y', labelcolor=color)
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+fig.savefig(
+    'Plot Valores de Saída: ' + 'beta_2_' + str(beta2) + '_alpha_' + str(alpha) + '_gamma_' + str(gamma) + '.png',
+    dpi=fig.dpi)
+
+data = pd.DataFrame({'Distância': nz_step,
+                     'Potência de saída': list_output_1,
+                     'Largura a meia altura': list_delta_1})
+data.to_csv('resultados_de_beta_2_' + str(beta2) + '_alpha_' + str(alpha) + '_gamma_' + str(gamma) + '.csv',
+            decimal=",")
