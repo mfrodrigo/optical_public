1588f3ec1cda5744be3550714ea75f077fe6948b
mfrodrigo <rodrigomf43@gmail.com>
2021-07-25 08:45:31 -0300

    add test for test_approximation_to_quasi_fermi_level

--- optical_amplifier/soa.py
+++ optical_amplifier/soa.py
@@ -2,7 +2,7 @@
 
 """
 import numpy as np
-from math import sqrt, exp, pi, log
+from math import sqrt, exp, pi, ceil
 from mpmath import sech
 
 
@@ -34,7 +34,8 @@ class SemiconductorOpticalAmplifier:
     dn1dn = -1.8e-26  # Differential of active region refractive index with respect to carrier density (m^-3)
     neq0 = 3.22  # Equivalent refractive index at zero carrier density (from (29))
     dneqdn = n1 * confine / (neq0 * (
-            2 - confine)) * dn1dn  # Differential of equivalent refractive index with respect to carrier density (equation (30)) (m^-3)
+            2 - confine)) * dn1dn  # Differential of equivalent refractive index with respect to carrier density (
+    # equation (30)) (m^-3)
     eta_in = 3  # Input coupling loss (dB)
     eta_out = 3  # Output coupling loss (dB)
     R1 = 5e-5  # Input facet reflectivity
@@ -52,33 +53,66 @@ class SemiconductorOpticalAmplifier:
     mhh = m0 * 0.46  # Effective mass of a heavy hole in the VB
     mlh = m0 * 0.056  # Effective mass of a light hole in the VB
 
+    L = (Lc + Lt) * 1e-6  # average length
+
+    r1 = sqrt(R1)  # amplitude reflectivities
+    r2 = sqrt(R2)
+
+    eta_in = 10 ** (-eta_in / 10)  # converting from dB to linear quantities
+    eta_out = 10 ** (-eta_out / 10)
+
+    neq = ((n1 ** 2 - n2 ** 2) * confine / (2 + confine) + n2 ** 2) ** 0.5
+    delta_Em = h * c / (2 * neq * L)  # longitudinal mode energy spacing (assumed carrier independent) (J)
+
     # parameters used in nilsson function
 
     mdh = (mhh ** 1.5 + mlh ** 1.5) ** (2 / 3)
     nc = 2 * (me * kT / (2 * pi * hbar ** 2)) ** 1.5
     nv = 2 * (mdh * kT / (2 * pi * hbar ** 2)) ** 1.5
 
-    def __init__(self):
-        pass
+    def __init__(self, number_spatial_divisions, number_spectrum_slices, wavelength_0, wavelength_1):
+        self.number_spatial_divisions = number_spatial_divisions
+        self.number_spectrum_slices = number_spectrum_slices
+        self.wavelength_0 = wavelength_0 * 1e-9
+        self.wavelength_1 = wavelength_1 * 1e-9
+        self.energy = None
+        self.delta_energy = None
+        self.calc_delta_energy_and_energy()
+
+    def calc_delta_energy_and_energy(self):
+        """
+        This function calculate delta_energy and energy.
+        """
+        lower_energy_limit = self.h * self.c / self.wavelength_1
+        upper_energy = self.h * self.c / self.wavelength_0
+        energy_spacing = (upper_energy - lower_energy_limit) / self.number_spectrum_slices
+        km = ceil(energy_spacing / self.delta_Em)
+        upper_energy = lower_energy_limit + self.number_spectrum_slices * km * self.delta_Em
+        self.delta_energy = (upper_energy - lower_energy_limit) / self.number_spectrum_slices
+        self.energy = np.arange(lower_energy_limit, upper_energy, self.delta_energy)
 
     def energy_gap(self, carrier_density):
         """
         this function calculate the approximation to band gap energy (J).
 
-        Returns:
+        Args:
+            carrier_density: (ndarray) Initial guess for carrier density.
 
+        Returns:
+            energy_gap: (ndarray) Energy gap.
         """
-        return self.e * (1.35 - 0.775 * self.yAs + 0.149 * self.yAs ** 2 - self.Kg * carrier_density**(1 / 3))
+        return self.e * (1.35 - 0.775 * self.yAs + 0.149 * self.yAs ** 2 - self.Kg * carrier_density ** (1 / 3))
 
     def approximation_to_quasi_fermi_level(self, carrier_density, band):
         """
-
+        this function calculate approximations to
+        quasi-Fermi level relative to band edge.
         Args:
-            carrier_density:
-            band:
+            carrier_density: (ndarray) Initial guess for carrier density.
+            band: (string) band specification (conduction or valence).
 
         Returns:
-
+            quase_fermi_level: (ndarray) approximation for quasi-fermi level.
         """
         if band == "conduction":
             delta = carrier_density / self.nc
@@ -89,17 +123,20 @@ class SemiconductorOpticalAmplifier:
 
         return self.kT * (np.log(delta) + delta * (64 + 0.05524 * delta * (64 + np.sqrt(delta))) ** -0.25)
 
-    def gain_coefficient(self, carrier_density, Energy):
+    def gain_coefficient(self, carrier_density):
         """
+        this function calculate the material gain coefficient
+        and additive spontaneous emission term.
 
         Returns:
-
+            material_gain_coefficient: (ndarray) Material gain coefficient.
+            additive_spontaneous_emission_term: (ndarray) Additive spontaneous emission term.
         """
         lafetime = (self.Arad + self.Brad * carrier_density) ** -1
         energy_gap = self.energy_gap(carrier_density)
-        energy_a = (Energy - energy_gap(carrier_density)) * \
+        energy_a = (self.energy - energy_gap) * \
                    self.mhh / (self.me + self.mhh)
-        energy_b = (Energy - energy_gap(carrier_density)) * \
+        energy_b = (self.energy - energy_gap) * \
                    self.me / (self.me + self.mhh)
 
         energy_fermi_conduction = \
@@ -109,21 +146,20 @@ class SemiconductorOpticalAmplifier:
         energy_fermi_valence = self.approximation_to_quasi_fermi_level(carrier_density=carrier_density,
                                                                        band='valence')
 
-        fermi_dirac_conduction = (1 + exp(energy_a - energy_fermi_conduction) /
+        fermi_dirac_conduction = (1 + np.exp(energy_a - energy_fermi_conduction) /
                                   self.kT) ** -1
-        fermi_dirac_valence = (1 + exp(energy_b - energy_fermi_valence) /
+        fermi_dirac_valence = (1 + np.exp(energy_b - energy_fermi_valence) /
                                self.kT) ** -1
 
-        gain_coefficient = []
-        absorption_coefficient = []
-        # for i in range(len(Energy)):
-        #     if Energy(i) - energy_gap:
-        #         gain_coefficient.append(
-        #             sqrt(Energy(i) - energy_gap) *\
-        #             fermi_dirac_conduction(i) * (1 - fermi_dirac_valence(i))\
-        #             / (Energy(i) ** 2))
-        #         absorption_coefficient.append(
-        #             sqrt(Energy(i) - energy_gap)*\
-        #             (1-fermi_dirac_conduction)*(fermi_dirac_valence(i))
-        #
-        #         )
+        aux = self.energy - energy_gap
+        aux[aux <= 0] = 0
+        gain_coefficient = np.sqrt(aux) * fermi_dirac_conduction * (1 - fermi_dirac_valence) * \
+                           self.energy ** 2
+        absorption_coefficient = np.sqrt(aux) * (1 - fermi_dirac_conduction) * fermi_dirac_valence * \
+                                 self.energy ** 2
+        gain_coefficient = self.K0 * gain_coefficient / lafetime
+        absorption_coefficient = self.K0 * absorption_coefficient / lafetime
+        material_gain_coefficient = gain_coefficient - absorption_coefficient
+        additive_spontaneous_emission_term = self.confine * gain_coefficient * self.delta_energy / self.h
+
+        return material_gain_coefficient, additive_spontaneous_emission_term
--- test/test_soa.py
+++ test/test_soa.py
@@ -10,8 +10,13 @@ class TestSOA:
     """
 
     """
-    soa = SemiconductorOpticalAmplifier()
 
+    lambda0 = 1300  # start wavelength for gain coefficient and ASE spectrum (nm)
+    lambda1 = 1650  # end wavelength for gain coefficient and ASE spectrum (nm)
+    soa = SemiconductorOpticalAmplifier(number_spatial_divisions=100,
+                                        number_spectrum_slices=100,
+                                        wavelength_0=lambda0,
+                                        wavelength_1=lambda1)
     carrier_density = np.ones(100) * 1.2e24
 
     @pytest.mark.parametrize('carrier_density', [carrier_density])
@@ -19,7 +24,7 @@ class TestSOA:
         """
         tests the function that calculates the energy gap.
         Args:
-            carrier_density: (ndarray)
+            carrier_density: (ndarray) Initial guess for carrier density.
         """
         expected = 122.83044e-19
         assert pytest.approx(np.sum(self.soa.energy_gap(carrier_density=carrier_density)),
@@ -29,6 +34,19 @@ class TestSOA:
                              [(carrier_density, 'conduction', 13.5808e-21),
                               (carrier_density, 'valence', -7.7117e-21)])
     def test_approximation_to_quasi_fermi_level(self, carrier_density, band, expected):
+        """
+        tests the function that calculates the approximation_to_quasi_fermi_level (nilsson).
+        Args:
+            carrier_density: (ndarray) Initial guess for carrier density.
+            band: (string) band specification (conduction or valence).
+            expected: (float) expected value
+        """
         answer = self.soa.approximation_to_quasi_fermi_level(carrier_density=carrier_density,
                                                              band=band)
         assert pytest.approx(answer[0], abs=1e-24) == expected
+
+    @pytest.mark.parametrize("carrier_density", [carrier_density])
+    def test_gain_coefficient(self, carrier_density):
+        material_gain_coefficient, additive_spontaneous_emission_term = \
+            self.soa.gain_coefficient(carrier_density=carrier_density)
+        assert 1 == 1
