aec56553214f258b0e5d661c5ef4e414c027211e
mfrodrigo <rodrigomf43@gmail.com>
2021-10-28 08:55:08 -0300

    filter

--- example/example4.py
+++ example/example4.py
@@ -1,68 +1,55 @@
 import math
 import numpy as np
 from pulse.pulse import Pulse
-from scipy import signal
-from pulse.prbs import prbs_7
-from channel.channel import Channel
-from scipy.fft import fft, fftfreq
-import plotly.graph_objects as go
-from output.plotter import Plotter
 import matplotlib.pyplot as plt
-from modulator.mach_zehnder_interferometer import MachZehnderInterferometer
-from scipy.signal import butter, lfilter
+from scipy.fft import fft, fftfreq, ifft
+from filter_design import transfer_function
 
 # dt
-T = 6000  # (ps) deve ser pelo 4x FWHM
+T = 1000e12
 num_samplesperbit = 2 ** 9  # should be 2^n
 dt = T / num_samplesperbit  # sampling time(ps) # time step (ps)
 pulse_number = 1
 t = (np.array(range(1, pulse_number * num_samplesperbit + 1)) - (pulse_number * num_samplesperbit + 1) / 2) * dt
-delta_1 = np.arange(-int((pulse_number + 1) / 2), 0) * num_samplesperbit + num_samplesperbit / 2
-delta_2 = np.arange(0, int(pulse_number / 2)) * num_samplesperbit + num_samplesperbit / 2
-t0 = np.concatenate((delta_1, delta_2))
-t0 = np.repeat(t0, num_samplesperbit) * dt
-t = t[0:512]*1e-12
-t0 = t0[0:512]*1e-12
 pulse = Pulse(
     power=1e-3,
     time=t,
     SNR_dB=30,
-    FWHM=1000*1e-12,
-    shift=t0
+    FWHM=100e12,
 )
+pulse = pulse.pulse[:, 0]
+fig = plt.figure()
+plt.plot(t, np.abs(pulse)**2)
+plt.xlabel('tempo (s)')
+plt.ylabel('gaussian pulse')
+plt.grid(True)
+fig.savefig("pulso_original.png")
 
-# array_prbs_7 = np.array(np.repeat(prbs_7(), num_samplesperbit, axis=0), dtype=complex)
-# pulse.pulse = array_prbs_7*pulse.pulse
-# fig = go.Figure()
-# fig.add_trace(go.Scatter(
-#             x=t,
-#             y=np.abs(pulse.pulse/np.max(np.abs(pulse.pulse)))[:, 0],
-#         ))
-# fig.show()
+sig_f = fft(pulse)
+f = fftfreq(num_samplesperbit, dt)
+fig = plt.figure()
+plt.plot(f, 2.0/num_samplesperbit * np.abs(sig_f))
+plt.grid()
+plt.xlabel('frequência (Hz)')
+plt.ylabel('Gaussian Pulse')
+plt.grid(True)
+fig.savefig("fft_gausssian_pulse.png")
 
-yf = fft(pulse.pulse)
-xf = fftfreq(num_samplesperbit, dt)[:num_samplesperbit//2]
-fig = go.Figure()
-fig.add_trace(go.Scatter(
-            x=xf,
-            y=np.abs(yf[:num_samplesperbit//2, 0]),
-        ))
-fig.show()
+sig_filter = ifft(sig_f*transfer_function(f))
+fig = plt.figure()
+plt.plot(t, np.abs(sig_filter)**2, label='Resposta ao filtro')
+plt.xlabel('tempo (s)')
+plt.ylabel('gaussian pulse')
+plt.legend()
+plt.grid(True)
+fig.savefig("pulso_filtrado.png")
 
-sos = signal.butter(4, 2*np.pi*10, 'low', fs=2*np.pi*22, output='sos')
-output_signal = signal.sosfilt(sos, pulse.pulse/np.max(np.abs(pulse.pulse)))
-output_signal = output_signal/np.max(np.abs(output_signal))
-fig = go.Figure()
-fig.add_trace(go.Scatter(
-            x=t,
-            y=np.abs(output_signal[:, 0])**2,
-            name='output',
-        ))
-fig.add_trace(go.Scatter(
-            x=t,
-            y=(np.abs(pulse.pulse/np.max(np.abs(pulse.pulse)))[:, 0]) **2,
-            name='input',
-        ))
-fig.show()
-
-print("OI")
+sig_f = fft(sig_filter)
+f = fftfreq(num_samplesperbit, 1/dt)
+fig = plt.figure()
+plt.plot(f, 2.0/num_samplesperbit * np.abs(sig_f))
+plt.grid()
+plt.xlabel('frequência (Hz)')
+plt.ylabel('Gaussian Pulse')
+plt.grid(True)
+fig.savefig("fft_pulso_filtrado.png")
