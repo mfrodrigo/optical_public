537637b09c333b07386e5fc58fef045ffb3e527a
mfrodrigo <rodrigomf43@gmail.com>
2022-02-13 15:33:47 -0300

    Finish test filter

--- example/example_pulse.py
+++ example/example_pulse.py
@@ -1,74 +1,32 @@
 import numpy as np
+
+from pulse.prbs import prbs_7
 from pulse.pulse import Pulse
 import matplotlib.pyplot as plt
 from filters.filter_design import butter_filter
 from output.plotter import Plotter
 
 # dt
-T = 1000e-12
+T = 1000e-12  # (ps) deve ser pelo 4x FWHM
 num_samplesperbit = 2001  # should be 2^n
 dt = T / num_samplesperbit  # sampling time(ps) # time step (ps)
-pulse_number = 1
-t = np.arange(0, pulse_number * num_samplesperbit) * dt
-t = t - pulse_number * num_samplesperbit * dt / 2
-pulse = Pulse(
-    power=1,
-    time=t,
-    SNR_dB=100,
-    FWHM=400e-12,
+pulse_number = 127*7
+t, t0 = Pulse.generate_time(
+    pulse_number, num_samplesperbit,
+    dt, T
 )
-pulse = pulse.pulse[:, 0]
+array_prbs_7 = np.repeat(prbs_7(), num_samplesperbit, axis=0) * 5
 
-Plotter.plot_pulse_input_and_output([[t, np.abs(pulse) ** 2, " pulso original"]],
-                                    graph_title="Pulso original no domínio do tempo",
-                                    x_graph="s",
-                                    y_graph='Watt'
-                                    )
-
-sig_f = np.fft.fftshift(np.fft.fft(pulse)) / len(pulse)
-f = np.linspace(-0.5, 0.5, len(pulse)) * 1 / dt
-
-Plotter.plot_pulse_input_and_output([[f, np.abs(sig_f) ** 2, " FFT pulso original"]],
-                                    graph_title="FFT pulso original",
-                                    x_graph="Hz",
-                                    y_graph='Watt'
-                                    )
-
-sig_filter = np.fft.ifftshift(np.fft.ifft(sig_f * butter_filter(
-    4, 1e9, fs=1 / dt, sig=f
-))) * len(sig_f)
-fig = plt.figure()
-plt.plot(t, np.abs(sig_filter) ** 2,
-         label='Resposta ao filtro')
-plt.xlabel('tempo (s)')
-plt.ylabel('gaussian pulse')
-plt.legend()
-plt.grid(True)
-fig.savefig("pulso_filtrado.png")
-
-sig_f_output = np.fft.fft(np.fft.fftshift(sig_filter)) / len(sig_filter)
-fig = plt.figure()
-plt.plot(f, np.abs(sig_f_output))
-plt.grid()
-plt.xlabel('frequência (Hz)')
-plt.ylabel('Gaussian Pulse')
-plt.grid(True)
-fig.savefig("fft_pulso_filtrado.png")
-#
-#
-Plotter.plot_pulse_input_and_output([[t, np.abs(pulse) ** 2, "input"],
-                                     [t, np.abs(sig_filter) ** 2, "output"]],
-                                    graph_title="Comparação dos pulsos de entrada e saída",
+Plotter.plot_pulse_input_and_output([[t, array_prbs_7[8004:12006, 0] / 5000, 'PRBS ']],
+                                    graph_title="pulso e onda quadrada",
                                     x_graph='s',
-                                    y_graph='Watt')
-Plotter.plot_pulse_input_and_output([[f, np.abs(sig_f) ** 2, 'input'],
-                                     [f, np.abs(sig_f_output) ** 2, 'output']],
-                                    graph_title="FFT dos pulsos de entrada e saída",
-                                    x_graph='Hz',
-                                    y_graph='Watt')
+                                    y_graph="")
 
-# Plotter.plot_pulse_input_and_output([[f, 20 * np.log10(np.abs(transfer_function(f))), 'filtro']],
-#                                     graph_title="Função de transferência",
-#                                     x_graph='Hz',
-#                                     y_graph='dB'
-#                                     )
+sig_filter = butter_filter(
+    4, 5e9, fs=1 / dt, sig=array_prbs_7[:, 0]
+)
+
+Plotter.plot_pulse_input_and_output([[t, sig_filter[8004:12006] / 5000, 'PRBS ']],
+                                    graph_title="pulso e onda quadrada",
+                                    x_graph='s',
+                                    y_graph="Pulso filtrado")
--- example/tests/teste_2.py
+++ example/tests/teste_2.py
@@ -1,23 +1,32 @@
 import numpy as np
+
+from pulse.prbs import prbs_7
+from pulse.pulse import Pulse
 import matplotlib.pyplot as plt
-from scipy import signal
+from filters.filter_design import butter_filter
+from output.plotter import Plotter
 
-fs = 1000  # Sampling frequency
-# Generate the time vector properly
-t = np.arange(1000) / fs
-signala = np.sin(2*np.pi*100*t) # with frequency of 100
-plt.plot(t, signala, label='a')
+# dt
+T = 1000e-12  # (ps) deve ser pelo 4x FWHM
+num_samplesperbit = 2001  # should be 2^n
+dt = T / num_samplesperbit  # sampling time(ps) # time step (ps)
+pulse_number = 127*7
+t, t0 = Pulse.generate_time(
+    pulse_number, num_samplesperbit,
+    dt, T
+)
+array_prbs_7 = np.repeat(prbs_7(), num_samplesperbit, axis=0) * 5
 
-signalb = np.sin(2*np.pi*20*t) # frequency 20
-plt.plot(t, signalb, label='b')
+Plotter.plot_pulse_input_and_output([[t, array_prbs_7[8004:12006, 0] / 5000, 'PRBS ']],
+                                    graph_title="Onda quadrada",
+                                    x_graph='s',
+                                    y_graph="")
 
-signalc = signala + signalb
-plt.plot(t, signalc, label='c')
+sig_filter = butter_filter(
+    1, 5e9, fs=1 / dt, sig=array_prbs_7[:, 0]
+)
 
-fc = 30  # Cut-off frequency of the filter
-w = fc / (fs / 2) # Normalize the frequency
-b, a = signal.butter(5, w, 'low')
-output = signal.filtfilt(b, a, signalc)
-plt.plot(t, output, label='filtered')
-plt.legend()
-plt.show()
+Plotter.plot_pulse_input_and_output([[t, sig_filter[8004:12006] / 5000, 'PRBS ']],
+                                    graph_title="Onda filtrada",
+                                    x_graph='s',
+                                    y_graph="")
