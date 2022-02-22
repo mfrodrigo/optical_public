4fc19b9f15bbfa3573b605d34a566a50eac5025e
mfrodrigo <rodrigomf43@gmail.com>
2022-02-22 06:57:03 -0300

    add electron-optic band

--- modulator/mach_zehnder_interferometer.py
+++ modulator/mach_zehnder_interferometer.py
@@ -10,23 +10,25 @@ class MachZehnderInterferometer:
 
     """
 
-    def __init__(self, pi_voltage_1, pi_voltage_2, electro_optical_band,
-                 sampling_frequency):
+    def __init__(self, pi_voltage_1, pi_voltage_2,
+                 sampling_frequency,
+                 N, cutoff):
         self.pi_voltage_1 = pi_voltage_1
         self.pi_voltage_2 = pi_voltage_2
-        self.electro_optical_band = electro_optical_band
         self.sampling_frequency = sampling_frequency
+        self.N = N
+        self.cutoff = cutoff
 
-    def electro_optical_response(self, input_signal):
+    def electro_optical_response(self, input_signal, ):
         output_signal = input_signal[:, 0]
-        f = linspace(-0.5, 0.5, len(output_signal)) * self.sampling_frequency
-        output_signal = fft.fftshift(fft.fft(output_signal)) / len(output_signal)
-        output_signal = fft.ifftshift(fft.ifft(output_signal * butter_filter(f))) * len(output_signal)
+        output_signal = butter_filter(self.N, self.cutoff,
+                                      self.sampling_frequency,
+                                      output_signal)
         return array(output_signal, ndmin=2).T
 
     def modulate(self, field, voltage_1, voltage_2):
-        # field = self.electro_optical_response(voltage_1)
+        voltage_1 = self.electro_optical_response(voltage_1)
         phase_shift_1 = voltage_1 * pi / self.pi_voltage_1
         phase_shift_2 = voltage_2 * pi / self.pi_voltage_2
         output_field = field * 0.5 * (exp(1j * phase_shift_1) + exp(1j * phase_shift_2))
-        return output_field
+        return output_field, voltage_1
