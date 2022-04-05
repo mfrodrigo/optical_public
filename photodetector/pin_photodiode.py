4eb3afe528351ab9821671061a6a01873ffc7717
mfrodrigo <rodrigomf43@gmail.com>
2022-04-04 21:38:04 -0300

    add new module

---
+++ photodetector/pin_photodiode.py
@@ -0,0 +1,13 @@
+"""
+This module has PIN photodetector implementation.
+"""
+
+
+class PinPhotodiode:
+
+    """
+    The PIN photodiode consists in a junction of p-n materials,
+    separated by an intrinsic material.
+    """
+    def __init__(self):
+        pass
