e1b9eda580f4c477be275e952e0ec45a6517f4b2
mfrodrigo <rodrigomf43@gmail.com>
2022-04-05 07:30:35 -0300

    add noise

--- photodetector/pin_photodiode.py
+++ photodetector/pin_photodiode.py
@@ -3,11 +3,15 @@ This module has PIN photodetector implementation.
 """
 import numpy as np
 
+
 class PinPhotodiode:
     """
     The PIN photodiode consists in a junction of p-n materials,
     separated by an intrinsic material.
     """
+    kb = 1.380649e-23  # J.K-1
+    T = 300  # K
+    q = 1.602176634e-19  # C
 
     def __init__(self, quantum_efficiency, wavelength,
                  area, bandwidth, dark_current,
@@ -24,11 +28,13 @@ class PinPhotodiode:
     def calc_responsivity(self):
         return self.quantum_efficiency * self.wavelength / 1.24
 
-    def calc_shot_noise(self):
-        pass
+    def calc_shot_noise(self, electric_current):
+        return np.sqrt(
+            2*self.q*(electric_current+self.dark_current)*self.bandwidth
+        )
 
     def calc_circuit_noise(self):
-        pass
+        return np.sqrt(4 * self.kb * self.T * self.noise_figure * self.bandwidth / self.load_resistance)
 
     def calc_electric_current(self, electric_field):
         incident_power = np.abs(electric_field) ** 2
